a0af78b8ec9b122c981befdc4a2208ef51241e92
Tejpreet Bal <tejpreetbal@gmail.com>
2023-05-27 12:10:19 -0600

    added 3-D array

--- school_data.py
+++ school_data.py
@@ -1,5 +1,5 @@
 # school_data.py
-# AUTHOR NAME
+# AUTHOR NAME Tejpreet Bal
 #
 # A terminal-based application for computing and printing statistics based on given input.
 # Detailed specifications are provided via the Assignment 3 git repository.
@@ -12,16 +12,34 @@ import numpy as np
 from given_data import year_2013, year_2014, year_2015, year_2016, year_2017, year_2018, year_2019, year_2020, year_2021, year_2022
 
 # Declare any global variables needed to store the data here
+single_arrays = [year_2013, year_2014, year_2015, year_2016, year_2017,
+                     year_2018, year_2019, year_2020, year_2021, year_2022]
 
 
 # You may add your own additional classes, functions, variables, etc.
+def create_3d_array(years):
+    """
+    Create a three-dimensional NumPy array by stacking the given years.
+
+    Args:
+        years (list): List of NumPy arrays representing each year's data.
+
+    Returns:
+        numpy.ndarray: A three-dimensional array representing the stacked data.
+    """
+    arr = np.stack(years, axis=0)
+    return arr 
+    
 
 
 def main():
     print("ENSF 592 School Enrollment Statistics")
 
     # Print Stage 1 requirements here
-
+    three_dimensional_arr = create_3d_array(single_arrays)
+    three_dimensional_arr = three_dimensional_arr.reshape(10,20,3)
+    print("Shape of full data array:", three_dimensional_arr.shape)
+    print("Dimensions of full data array:",three_dimensional_arr.ndim)
     # Prompt for user input
 
     # Print Stage 2 requirements here
